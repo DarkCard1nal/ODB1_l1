37bb0ac3465de7a6466d2bc4b1f8df009aa5c465
VX <fhjksgadl@gmail.com>
2024-09-07 20:21:02 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		Console.InputEncoding = Encoding.Unicode;
+		Console.OutputEncoding = Encoding.Unicode;
+
+		Console.WriteLine("str: " + Console.ReadLine());
+	}
+}
