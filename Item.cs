96d525475cd5857037304ea1c5f4ed9c0708a8e2
DarkCard1nal <34416583+DarkCard1nal@users.noreply.github.com>
2024-09-08 11:34:16 +0300

    Create class ItemsList

--- Item.cs
+++ Item.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace l1
+﻿namespace l1
 {
 	internal class Item
 	{
---
+++ ItemsList.cs
@@ -0,0 +1,32 @@
+﻿namespace l1
+{
+	internal class ItemsList
+	{
+		private List<Item> _items;
+
+		public ItemsList()
+		{
+			_items = new List<Item>();
+		}
+
+		public ItemsList(List<Item> items)
+		{
+			_items = new List<Item>(items);
+		}
+
+		public void Add(Item item)
+		{
+			_items.Add(item);
+		}
+
+		public void Clear()
+		{
+			_items.Clear();
+		}
+
+		public Item GetItem(int index)
+		{
+			return _items[index];
+		}
+	}
+}
